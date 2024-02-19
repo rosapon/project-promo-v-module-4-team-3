185ded5da410ca381be5c3c44a6f83085d0a11d5
Mbueno992 <mbueno_92@hotmail.com>
2024-02-19 12:57:04 +0100

    static route done

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://mbueno992.github.io/project-promo-v-module-3-team-3/',
+  base: '/',
 });
